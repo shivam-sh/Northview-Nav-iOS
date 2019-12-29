6ff309f1ff9f3cdc68e18090bb43f6c66e6cdf07
Mikayel Sargsyan <mikayelsarg@gmail.com>
2019-12-29 17:43:36 -0500

    Testing

---
+++ Northview Nav/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Northview Nav
+//
+//  Created by Artur Sargsyan on 2019-12-29.
+//  Copyright © 2019 Shivam Sharma. All rights reserved.
+//
+
+import Foundation
+
+// API keys can be generated at <http://developer.indooratlas.com/applications>
+let kAPIKey = "7c7b1355-dd0c-4113-90ed-0c343733b997"
+let kAPISecret = "vw1ZVQEQ3xM6wmPAICCuxCs07YVbLbfCTlg6xrUGkAYWWbOH6BAJxH5JKnk3oJrYq0uYiREbZf+liZeF5GOGlzIyDPalZxLffGdRgDqlDU0CQNIcPsB19L0zlKnMtA=="
